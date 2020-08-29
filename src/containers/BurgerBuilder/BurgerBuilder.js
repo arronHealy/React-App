faff20bc6a536f29c3f1d864f2952b9d3056e1c1
arronHealy <G00346883@gmit.ie>
2020-08-29 18:46:16 +0100

    added project so far

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Auxilary from '../../hoc/Auxilary';
+
+class BurgerBuilder extends Component {
+
+    render() {
+        return (
+            <Auxilary>
+                <div>Burger</div>
+                <div>Build controls</div>
+            </Auxilary>
+        );
+    }
+}
+
+export default BurgerBuilder;

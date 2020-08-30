b277ce20e602920ee19b35eb957907c04821444d
arronHealy <G00346883@gmit.ie>
2020-08-30 16:06:20 +0100

    added more components and logic to build a burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 import Auxilary from '../../hoc/Auxilary';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
+    addIngredientHandler = (type) => {
+
+    };
 
     render() {
         return (
             <Auxilary>
-                <div>Burger</div>
-                <div>Build controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
             </Auxilary>
         );
     }
